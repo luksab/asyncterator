0a27e38c47fd9c280aa4da5112583461699f5d85
luksab <lukas@sabatschus.de>
2022-12-15 13:10:15 +0100

    add scoped threads

--- src/lib.rs
+++ src/lib.rs
@@ -1,28 +1,30 @@
-use std::thread;
+use std::thread::{Scope, ScopedJoinHandle};
 
 /// A Asyncterator of type T
 ///
 /// The Asyncterator is a wrapper around an Iterator of type T.
 /// It spawns a thread, which executes the next() method of the inner iterator.
 /// The next() method of the Asyncterator returns the cached value of the inner iterator.
-pub struct Asyncterator<T>
+///
+/// It requires scoped threads.
+pub struct Asyncterator<'a, T>
 where
     T: Iterator + std::marker::Send,
 {
-    thread: thread::JoinHandle<T>,
+    thread: ScopedJoinHandle<'a, T>,
     receiver: std::sync::mpsc::Receiver<Option<T::Item>>,
     sender: std::sync::mpsc::Sender<()>,
 }
 
-impl<T> Asyncterator<T>
+impl<'a, T> Asyncterator<'a, T>
 where
     T: Iterator + std::marker::Send + 'static,
     <T as std::iter::Iterator>::Item: std::marker::Send,
     <T as std::iter::Iterator>::Item: 'static,
 {
     /// Create a new Asyncterator
-    pub fn new(inner: T) -> Self {
-        let thread = spaw_thread(inner);
+    pub fn new(inner: T, scope: &'a Scope<'a, '_>) -> Self {
+        let thread = spaw_thread(inner, scope);
         Self {
             thread: thread.0,
             receiver: thread.1,
@@ -35,10 +37,11 @@ where
     }
 }
 
-fn spaw_thread<T>(
+fn spaw_thread<'a, T>(
     inner: T,
+    scope: &'a Scope<'a, '_>,
 ) -> (
-    thread::JoinHandle<T>,
+    ScopedJoinHandle<'a, T>,
     std::sync::mpsc::Receiver<Option<T::Item>>,
     std::sync::mpsc::Sender<()>,
 )
@@ -49,7 +52,7 @@ where
 {
     let (main_sender, thead_receiver) = std::sync::mpsc::channel();
     let (thead_sender, main_receiver) = std::sync::mpsc::channel();
-    let thread = thread::spawn(move || {
+    let thread = scope.spawn(move || {
         let mut inner = inner;
         // wait for messages on thead_receiver
         for _ in thead_receiver {
@@ -63,7 +66,7 @@ where
     (thread, main_receiver, main_sender)
 }
 
-impl<T> Iterator for Asyncterator<T>
+impl<'a, T> Iterator for Asyncterator<'a, T>
 where
     T: Iterator + std::marker::Send,
 {
@@ -81,17 +84,19 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let mut iter = Asyncterator::new(0..10);
-        assert_eq!(iter.next(), Some(0));
-        assert_eq!(iter.next(), Some(1));
-        assert_eq!(iter.next(), Some(2));
-        assert_eq!(iter.next(), Some(3));
-        assert_eq!(iter.next(), Some(4));
-        assert_eq!(iter.next(), Some(5));
-        assert_eq!(iter.next(), Some(6));
-        assert_eq!(iter.next(), Some(7));
-        assert_eq!(iter.next(), Some(8));
-        assert_eq!(iter.next(), Some(9));
-        assert_eq!(iter.next(), None);
+        std::thread::scope(|s| {
+            let mut iter = Asyncterator::new(0..10, s);
+            assert_eq!(iter.next(), Some(0));
+            assert_eq!(iter.next(), Some(1));
+            assert_eq!(iter.next(), Some(2));
+            assert_eq!(iter.next(), Some(3));
+            assert_eq!(iter.next(), Some(4));
+            assert_eq!(iter.next(), Some(5));
+            assert_eq!(iter.next(), Some(6));
+            assert_eq!(iter.next(), Some(7));
+            assert_eq!(iter.next(), Some(8));
+            assert_eq!(iter.next(), Some(9));
+            assert_eq!(iter.next(), None);
+        })
     }
 }
